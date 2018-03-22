0a221165597cdbe11a314a786bfd082b22a4b48f
DESKTOP-I9AC578\ACER <mario.velyov@gmail.com>
2018-03-22 09:57:56 +0200

    created tests for generic data

--- test/data/generic.data.test.js
+++ test/data/generic.data.test.js
@@ -10,28 +10,91 @@ const Data = require('../../app/data/generic.data');
 
 describe('Generic data', () => {
 
-    const Model = {
-        findAll() {
+    let data = null;
+    let Model = null;
+    // beforeEach(() => {
+        Model = {
+            findAll: () => {},
+            findById: (id) => [],
+            create: (object) => [],
+        }
+        data = new Data(Model);
         
-        },
-        findById(id) {
-
-        }, 
-        create(newObject) {
-
-        }    
-    }
+    // })
 
     describe('getAll()', () => {
-        when('valid', () => {
-            it ('when no data exists, expect empty array', async() => {
+        describe('when valid', () => {
+            it ('when no data exists, expect to return empty array', async() => {
                 
+                // sinon.stub(Model, 'findAll')
+                // .returns([]);
+                Model.findAll = () => {
+                    return [];
+                };
+                
+                const objects = await data.getAll();
+
+                expect(objects).to.be.empty;
             })
-            it ('when object exists, expect to be returned', async() => {
+            it ('when object exists, expect to return the object', async() => {
+                const object = ['test', 1, 2, 3]
+
+                // sinon.stub(Model, 'findAll')
+                // .returns(objects);
+                Model.findAll = () => {
+                    return object;
+                };
                 
+
+                const resultObjects = await data.getAll();
+
+                expect(resultObjects).deep.equal(object);
             })
         })
-        when('invalid', () => {
+        describe('when invalid', () => {
+            
+        })
+   })
+
+    describe('getById()', () => {
+        describe('when valid', () => {
+            it ('existing id, expect to return the object', async() => {
+                const id = 1;
+                const object = {
+                    id: 1,
+                };
+
+                // sinon.stub(Model, 'findById')
+                // .returns(object)
+                Model.findById = (id) => {
+                    return object;
+                };
+
+                const resultObject = await data.getById(id);
+
+                expect(resultObject).to.exist;
+                expect(resultObject.id).to.equal(id);
+            })
+        })
+        describe('when invalid', () => {
+            it('non-existing id, expect to return null', async () => {
+                // sinon.stub(Model, 'findById')
+                // .returns(null);
+                Model.findById = (id) => {
+                    return null;
+                };
+
+                const resultObject = await data.getById(1);
+
+                expect(resultObject).to.be.null;
+            })
+        })
+    })
+    describe('create()', () => {
+        describe('when valid', () => {
+            
+        })
+        describe('when invalid', () => {
             
         })
     })
